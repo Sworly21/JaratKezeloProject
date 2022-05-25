a7decd5e882865e91ef7ad4eab8462248b2d7201
Szabó Bence <mentawary@gmail.com>
2022-05-25 16:14:11 +0200

    Óra vége

--- JaratKezeloProject/JaratKezelo.cs
+++ JaratKezeloProject/JaratKezelo.cs
@@ -1,8 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace JaratKezeloProject
 {
 	public class JaratKezelo
 	{
+		public void UjJarat(string jaratSzam, string repterHonnan, string repterHova, DateTime indulas)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void keses(string jaratSzam, int keses)
+		{
+			throw new NotImplementedException();
+		}
+
+		public DateTime mikorIndul(string jaratSzam)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<string> jaratokRepuloterrol(string repter)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
--- TestJaratKezeloProject/JaratKezeloTest.cs
+++ TestJaratKezeloProject/JaratKezeloTest.cs
@@ -1,18 +1,30 @@
 using NUnit.Framework;
+using JaratKezeloProject;
+using System;
 
 namespace TestJaratKezeloProject
 {
 	public class JaratKezeloTest
 	{
+		JaratKezelo kezelo;
+
 		[SetUp]
 		public void Setup()
 		{
+			kezelo = new JaratKezelo();
 		}
 
 		[Test]
-		public void Test1()
+		public void UjJaratDuplikaltJaratszam()
 		{
-			Assert.Pass();
+			kezelo.UjJarat("viz-123","Budapest","Praga",new DateTime(2022,05,25,16,00,00));
+			Assert.Throws<ArgumentException>(()=> {kezelo.UjJarat("viz-123", "Barcelona", "Velence", new DateTime(2022, 05, 26, 16, 00, 00)); });
+	
+		}
+
+		public void UjJaratUres()
+		{
+			
 		}
 	}
 }
